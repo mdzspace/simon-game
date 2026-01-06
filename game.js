e439e718dd0442f14bc7f7c432e3888a640f6706
mdzspace <madzsalamat@gmail.com>
2026-01-06 23:05:20 +0800

    sequencing with animation

--- game.js
+++ game.js
@@ -1,14 +1,17 @@
-var gamePattern = [];
+
 var buttonColors = ["red", "blue", "green", "yellow"];
 
+var gamePattern = [];
+
 function nextSequence() {
-    
-    var randomNumber = Math.floor(Math.random() * 4);
+  var randomNumber = Math.floor(Math.random() * 4);
 
-    var randomChosenColor = buttonColors[randomNumber];
+  var randomChosenColor = buttonColors[randomNumber];
 
-    gamePattern.push(randomChosenColor);
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
 }
-
-
-
