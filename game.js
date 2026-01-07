34f684b3ab1a69d348586c7afa13f9fc90b7c51b
mdzspace <madzsalamat@gmail.com>
2026-01-07 18:22:08 +0800

    game start

--- game.js
+++ game.js
@@ -1,9 +1,37 @@
-
-var buttonColors = ["red", "blue", "green", "yellow"];
+ var buttonColors = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
+var userClickedPattern = [];
+
+var gameStarted = false;
+var level = 0;
+
+$(".btn").on("click", function() {
+
+  var userChosenColor = ($(this).attr("id"));
+
+  userClickedPattern.push(userChosenColor);
+
+  playSound(userClickedPattern);
+
+  animatePress(userChosenColor);
+
+});
+
+$(document).keydown(function() {
+    if (!gameStarted) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        gameStarted = true;
+    }
+    
+});
 
 function nextSequence() {
+
+    level++
+    $("#level-title").text("Level " + level);
+    
   var randomNumber = Math.floor(Math.random() * 4);
 
   var randomChosenColor = buttonColors[randomNumber];
@@ -12,6 +40,20 @@ function nextSequence() {
 
   $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
 
-  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  playSound(randomChosenColor);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
+}
+
+function animatePress(currentColor) {
+
+  $("#" + currentColor).addClass("pressed");
+
+  setTimeout(function() {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+
 }
