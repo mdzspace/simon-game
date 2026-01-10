25b32f9d9d4dfafe06f3e5f316581b4a5bb3c6e0
mdzspace <madzsalamat@gmail.com>
2026-01-10 16:13:11 +0800

    complete and working

--- game.js
+++ game.js
@@ -6,32 +6,61 @@ var userClickedPattern = [];
 var gameStarted = false;
 var level = 0;
 
+// Detects click, saves the sequence of clicks
 $(".btn").on("click", function() {
 
   var userChosenColor = ($(this).attr("id"));
-
   userClickedPattern.push(userChosenColor);
 
-  playSound(userClickedPattern);
-
+  playSound(userChosenColor);
   animatePress(userChosenColor);
+
+  checkAnswer(userClickedPattern.length - 1);
 
 });
 
+// Detects keypress
 $(document).keydown(function() {
     if (!gameStarted) {
         $("#level-title").text("Level " + level);
         nextSequence();
         gameStarted = true;
     }
-    
+   
 });
 
+// Checks the sequence of clicks if it matches the game pattern sequence
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (gamePattern.length === userClickedPattern.length) {
+
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    var wrongAudio = new Audio('sounds/wrong.mp3');
+    wrongAudio.play();
+
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    startOver();
+  }
+}
+
+// Allows the program to create the next sequence of game pattern
 function nextSequence() {
 
-    level++
-    $("#level-title").text("Level " + level);
-    
+  userClickedPattern = [];
+
+  level++
+  $("#level-title").text("Level " + level);
+   
   var randomNumber = Math.floor(Math.random() * 4);
 
   var randomChosenColor = buttonColors[randomNumber];
@@ -43,6 +72,14 @@ function nextSequence() {
   playSound(randomChosenColor);
 }
 
+// Resets the game
+function startOver() {
+  gameStarted = false;
+  level = 0;
+  gamePattern = [];
+}
+
+// Effects:
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
